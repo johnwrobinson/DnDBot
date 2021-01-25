19ddd9d2f6e0a3491e0fed94439fc258ffcea613
Ieuan Lewis <ieuanlewis@kubrickgroup.com>
2021-01-25 21:31:35 +0000

    haheen and john have small legs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import random
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -25,31 +26,29 @@ async def on_message(message):
         await message.channel.send('There is no helping you.')
 
     if message.content.startswith('!woof'):
-        copy_pasta = ("What the fuck did you just fucking say about me, you "
-        "little bitch? I'll have you know I graduated top of my class in the "
-        "Navy Seals, and I've been involved in numerous secret raids on "
-        "Al-Quaeda, and I have over 300 confirmed kills. I am trained in "
-        "gorilla warfare and I'm the top sniper in the entire US armed forces. "
-        "You are nothing to me but just another target. I will wipe you the "
-        "fuck out with precision the likes of which has never been seen before "
-        "on this Earth, mark my fucking words. You think you can get away with "
-        "saying that shit to me over the Internet? Think again, fucker. As we "
-        "speak I am contacting my secret network of spies across the USA and "
-        "your IP is being traced right now so you better prepare for the storm"
-        ", maggot. The storm that wipes out the pathetic little thing you call "
-        "your life. You're fucking dead, kid. I can be anywhere, anytime, and I"
-        " can kill you in over seven hundred ways, and that's just with my bare"
-        " hands. Not only am I extensively trained in unarmed combat, but I "
-        "have access to the entire arsenal of the United States Marine Corps "
-        "and I will use it to its full extent to wipe your miserable ass off "
-        "the face of the continent, you little shit. If only you could have "
-        "known what unholy retribution your little \"clever\" comment was about"
-        " to bring down upon you, maybe you would have held your fucking "
-        "tongue. But you couldn't, you didn't, and now you're paying the price,"
-        " you goddamn idiot. I will shit fury all over you and you will drown "
-        "in it. You're fucking dead, kiddo.")
+        copy_pasta = ("What the fuck did you ...")
         await message.channel.send(copy_pasta)
         await message.channel.send("I mean..")
         await message.channel.send("Woof woof.")
         
+
+    if message.content.startswith('!event'):
+        box = message.content[7:].split(' ')
+
+        if box[0] == 'encounter':
+            foes =random.randint(1,10)
+            send_str = "You are attacked by " + str(foes) + ' ' + box[1]
+            await message.channel.send(send_str)
+
+        if box[0] == 'NPC':
+            age = random.choice(['young ' , ' middle aged ' , 'old '])
+            gender = random.choice(['female ', 'male '])
+            race = random.choice(['elf ' , ' dwarf ' , 'gnome '])
+            carry = random.choice(['A pair of rabbits ' , 'A hammer ', 'A large sack ', 'A push cart ', 'A pack ladden animal '])
+            sports = random.choice(['Crazy hair ' , ' extreme build ' , 'scars ' , ' disablity ' ,' extreme build'])
+            accent = random.choice(['suprisingly plain ' , ' softspoken like an asmr vid ' , 'boisterous ' , 'quickly stumbling over there words ' 
+            ,'slowly like an ent ', 'a deep accent from a far of land ', 'they numble and stutter ', 'they like to cuss'])
+            send_str = "a "  + age + gender + race + " approaches you they carry with them " +carry+ "you instantly notice there "+sports+"but when they open there mouth there accent shocks you, they are " + accent
+            await message.channel.send(send_str)
+
 client.run(os.getenv("TOKEN"))
